2729ab73eaa64ab3b089d1c96648da0be5435c22
Adam Gent <adam.gent@evocatus.com>
2012-10-31 01:00:56 -0400

    Added postgresql creation script.

---
+++ jirm-spring/src/test/sql/create-test-db.sql
@@ -0,0 +1,42 @@
+-- Table: test_bean
+
+-- DROP TABLE test_bean;
+
+CREATE TABLE test_bean
+(
+  string_prop text NOT NULL,
+  long_prop bigint,
+  timets timestamp without time zone,
+  CONSTRAINT string_prop_key PRIMARY KEY (string_prop)
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+-- Table: parent_bean
+
+-- DROP TABLE parent_bean;
+
+CREATE TABLE parent_bean
+(
+  id text,
+  test text
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+-- Table: grand_parent_bean
+
+-- DROP TABLE grand_parent_bean;
+
+CREATE TABLE grand_parent_bean
+(
+  id text,
+  parent text
+)
+WITH (
+  OIDS=FALSE
+);
